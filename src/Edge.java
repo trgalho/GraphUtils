9389f1329c00915a15b9454a08d54e2e2c653d95
Talisson Galho <trgalho@gmail.com>
2020-03-23 21:19:55 -0300

    Creation of helper struct-like class Edge.

---
+++ src/Edge.java
@@ -0,0 +1,13 @@
+/**
+ * Struct-like class for represent a graph edge
+ */
+class Edge {
+    /** Identifier of this edge */
+    public int id;
+
+    /** Target node id */
+    public int targetId;
+
+    /** Source node id */
+    public int sourceId;
+}
